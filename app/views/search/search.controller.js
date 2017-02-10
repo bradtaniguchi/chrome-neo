0aa03e3c5e09f3547d66e3f6409958c1c72e7c72
Brad <bradtaniguchi@gmail.com>
2017-02-09 16:10:50 -0800

    Added basic structure for search view

--- app/views/search/search.controller.js
+++ app/views/search/search.controller.js
@@ -1,6 +1,13 @@
 angular.module('chrome-neo').controller('SearchController', SearchController);
 SearchController.$inject = ['$log'];
-function SearchController() {
+function SearchController($log) {
   var vm = this;
+  vm.query = "";
+  vm.search = search;
   return vm;
+  /*function definitions*/
+  function search(query) {
+    $log.log("Searching query..." + query);
+    vm.query = ""; //clear the search query
+  }
 }
