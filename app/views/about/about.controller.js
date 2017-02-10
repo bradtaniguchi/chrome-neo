b9ba187b7cc11359199ab04442f3029265972765
Brad <bradtaniguchi@gmail.com>
2017-02-09 16:42:43 -0800

    Added some basic about files.
    I might change this to a new section view. Its rather large and
    uneccessary.

--- app/views/about/about.controller.js
+++ app/views/about/about.controller.js
@@ -1,6 +1,30 @@
 angular.module('chrome-neo').controller('AboutController', AboutController);
 AboutController.$inject = ['$log'];
-function AboutController() {
+function AboutController($log) {
   var vm = this;
+  vm.technologies = [];
+  vm.$onInit = onInit;
+
   return vm;
+  /*function definitions*/
+  function onInit() {
+    vm.technologies = [
+      {
+        'title': "angularjs",
+        'description' : "Front-end framework for single web pages.",
+        'link': "https://angularjs.org/"
+      },
+      {
+        'title': "Angular Material",
+        'description' : "Google's front-end material design for Angular.",
+        'link': "google.com"
+      },
+      {
+        'title': "Nodejs",
+        'description' : "Utilized for a build tool, with gulp.",
+        'link': "nodejs.org"
+      }
+
+    ];
+  }
 }
