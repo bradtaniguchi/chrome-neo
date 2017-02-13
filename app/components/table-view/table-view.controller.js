fc25157282bcf8032dc44070379e4c430f8a3ec0
Bradley Taniguchi <bradtaniguchi@gmail.com>
2017-02-13 00:53:22 +0000

    Added some mock data to display
    Need to start modularizing the code for the table views

--- app/components/table-view/table-view.controller.js
+++ app/components/table-view/table-view.controller.js
@@ -4,12 +4,32 @@ TableViewController.$inject=['$log', '$mdDialog', 'constants', 'AsterankService'
 
 function TableViewController($log, $mdDialog, constants, AsterankService) {
   var vm = this;
+  vm.chart = {};
+  console.log("In Controller");
   vm.close = close;
-  vm.test = test;
+  //vm.test = test;
   
+  onInit(); //call the function
   return vm;
   
   /*function definitons*/
+  function onInit() {
+    $log.log("Loading data...");
+    /*
+    Todo: add a loader service here
+    https://api.nasa.gov/api.html#NeoWS
+    */
+    /*define our chart object*/
+    vm.chart = {
+      data: [[10,20,30]],
+      labels: ["one", "two", "three"],
+      options: {
+        tooltips: {
+          enabled: false
+        }
+      }
+    };
+  }
   function close() {
     $mdDialog.hide();
   }
