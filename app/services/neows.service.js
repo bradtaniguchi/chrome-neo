9d4ac759804070893785f8b8146f71e81d9ba006
Bradley Taniguchi <bradtaniguchi@gmail.com>
2017-02-14 07:22:51 +0000

    Added working api usage to main page
    Still using the demo key, but the proccess works.

--- app/services/neows.service.js
+++ app/services/neows.service.js
@@ -1,12 +1,23 @@
 angular.module('chrome-neo').factory('NeoWsService', NeoWsService);
-NeoWsService.$inject = [];
-function NeoWsService() {
+NeoWsService.$inject = ['$log', '$http', 'constants'];
+function NeoWsService($log, $http, constants) {
+  var key;
   return {
-    test: test
+    test: test,
+    getWeekly : getWeekly
   };
   /*function definitions*/
   function test(callback) {
     /*test to see if you can make a connection to the service.*/
     callback(true);
+  }
+  function getWeekly() {
+    return $http({
+      url: constants.NEOWS_BASE_URL,
+      method: 'GET',
+      params: {
+        api_key : 'DEMO_KEY' //don't use the DEMO key too much!
+      }
+    });
   }
 }
