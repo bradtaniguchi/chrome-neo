a27bd08a978f72c1e65e00de5ec258a779a3ba3f
Bradley Taniguchi <bradtaniguchi@gmail.com>
2017-02-21 04:20:50 +0000

    Added basic setup for stat page, line graph
    The line graph is for NEOs provided from the asterank and neows service

--- app/views/stats/stats.controller.js
+++ app/views/stats/stats.controller.js
@@ -1,9 +1,37 @@
 angular.module('chrome-neo').controller('StatsController', StatsController);
-StatsController.$inject = ['$log'];
+StatsController.$inject = ['$log', 'AsterankService'];
 
-function StatsController() {
+function StatsController($log, AsterankService) {
   var vm = this;
-  vm.data = [100,300,200];
-  vm.labels = [];
+  var originatorEv;
+  
+  /*This is for days of the week*/
+  vm.lineLabels = ['Monday', 'Tuesday', 'Wednesday'];
+  vm.lineSeries = ['NEOs'];
+  /*NEOs per day*/
+  vm.lineData=[
+    [20,30,40]
+  ];
+  vm.options={};
+  vm.getNeos = getNeos;
+  vm.changeStat = changeStat;
+  
   return vm;
+  /*function defintions*/
+  function openMenu($mdMenu, ev) {
+    $log.log("Clicked");
+    originatorEv = ev;
+    $mdMenu.open(ev);
+  }
+  
+  function changeStat() {
+    return [];
+  }
+  /*
+  gets the amount of neos for the week, returns a list of 
+  */
+  function getNeos() {
+    
+    return [];
+  }
 }
