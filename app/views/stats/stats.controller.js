f6bb495995bb6f550a5257bbc7f7eb10f6d120a7
Brad <bradtaniguchi@gmail.com>
2017-02-21 16:22:01 -0800

    Added filter list implimentation for stats.ctrl
    Now there is a filter add, remove buttons that add and remove filters
    which will be applied when searching the database in the stat view.
    Next steps will be to work on the other canvas types.
    I may move the tab list format below the filters, this way the graph
    will change but the filters will stay.

--- app/views/stats/stats.controller.js
+++ app/views/stats/stats.controller.js
@@ -1,10 +1,21 @@
 angular.module('chrome-neo').controller('StatsController', StatsController);
-StatsController.$inject = ['$log', 'AsterankService', '$mdMenu'];
+StatsController.$inject = [
+  '$log',
+  'AsterankService',
+  '$mdMenu',
+  'queryFields'
+];
 
-function StatsController($log, AsterankService, $mdMenu) {
+function StatsController($log, AsterankService, $mdMenu, queryFields) {
   var vm = this;
   var originatorEv;
-  
+  /*This is for the datapoint filter options*/
+  vm.menuOptions = Object.keys(queryFields);
+  vm.newFilter= ""; //the currently selected filter view
+  vm.newValue = "";// the currently entered value
+  /*list of all the filters in the view*/
+  vm.filters = [];
+
   /*This is for days of the week*/
   vm.lineLabels = ['Monday', 'Tuesday', 'Wednesday'];
   vm.lineSeries = ['NEOs'];
@@ -13,27 +24,70 @@ function StatsController($log, AsterankService, $mdMenu) {
     [20,30,40]
   ];
   vm.options={};
-  
+
   vm.getNeos = getNeos;
   vm.changeStat = changeStat;
-  vm.openMenu = vm.openMenu;
-  
+  vm.openMenu = openMenu;
+  vm.addFilter = addFilter;
+  vm.removeFilter = removeFilter;
+  vm.$onInit = onInit;
+
   return vm;
   /*function defintions*/
+  function onInit() {
+  }
+  /**
+   * Open menu handler, opens the menu of filter options
+   * @param  {[type]} $mdMenu [description]
+   * @param  {[type]} ev      [description]
+   * @return {[type]}         [description]
+   */
   function openMenu($mdMenu, ev) {
     $log.log("Clicked");
     originatorEv = ev;
     $mdMenu.open(ev);
   }
-  
+  /**
+   * Adds a new filter to the filter list, with the given name
+   * @param {string} filterName name of the filter to apply, this will actually
+   * be parsed by the Asterank service, so it MUST be a valid field
+   * @param {string} filterValue value for the given filter, this will actually
+   * be parsed by the Asterank service, so it MUST be a valid field
+   */
+  function addFilter(filterName, filterValue) {
+    /*first make sure the filters are filled..*/
+    if(filterName !== "" && filterValue !== ""){
+      /*adding filter..*/
+      $log.log("Adding filter ");
+
+      vm.filters.push({
+        "filterName": filterName,
+        "filterValue": filterValue
+      });
+      vm.newFilter = "";
+      vm.newValue = "";
+    }
+  }
+  /**
+   * Removes the given filter at the index from the vm.filters list
+   * @param  {string} index the index of the filter to remove
+   */
+  function removeFilter(index) {
+    vm.filters.splice(index, 1);
+  }
+  /**
+   * Changes the chart type to the new settings
+   * @return {[type]} [description]
+   */
   function changeStat() {
+    $log.log("changing stat view");
     return [];
   }
-  /*
-  gets the amount of neos for the week, returns a list of 
-  */
+  /**
+   * Gets the data points from the API endpoint
+   */
   function getNeos() {
-    
+
     return [];
   }
 }
--- test/controllers/stats.spec.js
+++ test/controllers/stats.spec.js
@@ -8,16 +8,35 @@
         StatsController = $controller('StatsController');
       });
     });
-    
+
     /*tests to make*/
     it('exists', function(){
       expect(StatsController).toBeDefined();
     });
-    
+
     it('check functions exist', function(){
       //expect(StatsController.openMenu.toBeDefined(); //needs dependencies
       expect(StatsController.changeStat).toBeDefined();
       expect(StatsController.getNeos).toBeDefined();
+      expect(StatsController.changeStat).toBeDefined();
+      expect(StatsController.openMenu).toBeDefined();
+      expect(StatsController.addFilter).toBeDefined();
+      expect(StatsController.removeFilter).toBeDefined();
+    });
+    it("check addFilter", function(){
+      StatsController.addFilter("filterName", "filterValue");
+      /*check to see if it did it*/
+      expect(StatsController.filters[0]).toBeDefined();
+      expect(StatsController.filters.length).toEqual(1);
+      expect(StatsController.filters[0].filterName).toEqual("filterName");
+      //expect(StatsController.filters.filterValue).toEqual("filterValue");
+    });
+    it("check removeFilter", function(){
+      StatsController.addFilter("filterName", "filterValue");
+      StatsController.removeFilter(0);
+
+      expect(StatsController.filters[0]).not.toBeDefined();
+      expect(StatsController.filters.length).toEqual(0);
     });
   });
 })();
