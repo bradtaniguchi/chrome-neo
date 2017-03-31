9189f19a72c9f768c3bab74b1a10aa64ddfcbc65
Brad <bradtaniguchi@gmail.com>
2017-03-31 11:46:51 -0700

    UI updates to main page and intersting neo
    Now you can go to the stat page for the most interesting neo, and the
    date section has been moved.

--- app/components/interesting-neo/interesting-neo.controller.js
+++ app/components/interesting-neo/interesting-neo.controller.js
@@ -9,18 +9,26 @@ angular.module('chrome-neo').component('interestingNeo', interestingNeo)
 .controller('InterestingNeoController', InterestingNeoController);
 InterestingNeoController.$inject=[
   '$log',
-  'ChromeService'
+  'ChromeService',
+  '$state'
 ];
 
-function InterestingNeoController($log, ChromeService) {
+function InterestingNeoController($log, ChromeService, $state) {
   var vm = this;
-  
+
   vm.jplDetail = jplDetail;
+  vm.stats = stats;
   return vm;
   /*function definitions*/
   function jplDetail(spkid){
     var rawUrl ='http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=';
     $log.log("looking up the given id in browser " + spkid);
     ChromeService.link(rawUrl + spkid);
+  }
+
+  function stats(spkid) {
+    $state.go('stats', {
+      id: spkid
+    });
   }
 }
