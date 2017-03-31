7718578fb4a7cc72f99b31da304a6ce2b63069f4
Brad <bradtaniguchi@gmail.com>
2017-03-31 12:23:06 -0700

    Added new component to handle jpl redirect
    The jpl redirect button takes 1 argument and redirects the user to jpls
    offical website. So I can redirect from the most interesting neo and the
    stat page.

---
+++ app/components/jpl-link/jpl-link.controller.js
@@ -0,0 +1,33 @@
+/**
+ * Component that take a single binding, which will transfer the user
+ * to JPL's offical page for the given NEO.
+ * @type {component}
+ */
+var jplLink = {
+  templateUrl: 'components/jpl-link/jpl-link.view.html',
+  controller: 'JplLinkController as vm',
+  bindings: {
+    spkid: '<'
+  }
+};
+angular.module('chrome-neo').component('jplLink', jplLink)
+.controller("JplLinkController", JplLinkController);
+
+JplLinkController.$inject=['$log','ChromeService'];
+function JplLinkController($log, ChromeService) {
+  $log.log('link controller loaded');
+  var vm = this;
+
+  vm.lookup = lookup;
+  return vm;
+  /*function defnitions*/
+  /**
+   * Opens the offical JPL page using ChromeSerivce.
+   * @param  {number} spkid id of the neo to lookup on jpls website
+   */
+  function lookup(){
+    var rawUrl ='http://ssd.jpl.nasa.gov/sbdb.cgi?sstr=';
+    $log.log("looking up the given id in browser " + vm.spkid);
+    ChromeService.link(rawUrl + vm.spkid);
+  }
+}
